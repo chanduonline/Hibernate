cca86d56dbbb270ec65576fffbd13bdc1dff60a7
Chandu <67627909+chanduonline@users.noreply.github.com>
2020-07-06 18:07:56 +0530

    Add files via upload
    Hibernate Table Per Concrete  Inheritance Application

---
+++ Hibernate_TPC_5/src/main/java/com/chandra/hibernate/pojo/RegularEmployee.java
@@ -0,0 +1,42 @@
+package com.chandra.hibernate.pojo;
+
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+
+@Entity  
+@Table(name="Regular_Employee_TPC")  
+@AttributeOverrides({  
+    @AttributeOverride(name="id", column=@Column(name="id")),  
+    @AttributeOverride(name="name", column=@Column(name="name"))  
+})  
+public class RegularEmployee extends Employee{
+	
+	@Column(name = "SALARY")
+	private float salary;
+	@Column(name = "BONUS")
+	private int bonus;
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+
+}
